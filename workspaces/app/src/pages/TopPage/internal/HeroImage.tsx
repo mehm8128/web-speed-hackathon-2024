4271a3f948acfc30aac7ec072d7c3e06228e8d05
mehm8128 <mehm8128@gmail.com>
2024-03-23 12:26:25 +0900

    heroimageを軽く

--- workspaces/app/src/pages/TopPage/internal/HeroImage.tsx
+++ workspaces/app/src/pages/TopPage/internal/HeroImage.tsx
@@ -2,7 +2,6 @@ import { useCallback, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { Mesh, OrthographicCamera, PlaneGeometry, Scene, ShaderMaterial, TextureLoader, WebGLRenderer } from 'three';
 
-import { IMAGE_SRC } from './ImageSrc';
 
 const _Wrapper = styled.div`
   aspect-ratio: 16 / 9;
@@ -47,7 +46,7 @@ export const HeroImage: React.FC = () => {
 
     const textureLoader = new TextureLoader();
 
-    textureLoader.load(IMAGE_SRC, (texture) => {
+    textureLoader.load('/assets/output.jpg', (texture) => {
       const geometry = new PlaneGeometry(2, 2);
       const material = new ShaderMaterial({
         fragmentShader: `uniform sampler2D tImage;
